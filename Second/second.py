70283aed8f6bcd72eacfd4b9f79441695929fa08
INE-01 <INE-01@Admins-iMac-5.local>
2020-07-29 18:50:37 +0700

    save

---
+++ Second/second.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column,Integer,String,Date 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:OKAekt15498@node1448-prakorn.app.ruk-com.cloud:11027/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	comment = Column(String)
+
+@app.route('/')
+def index():
+	result = Comments.query.all()
+
+	return render_template('index.html', result=result)
+
+@app.route('/sign')
+def sign():
+	return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+	name = request.form['name']
+	comment = request.form['comment']
+	signature = Comments(name=name, comment=comment)
+	db.session.add(signature)
+	db.session.commit()
+
+	return redirect(url_for('index'))
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org', 'https://www.enkato.com']
+	return render_template('example.html', links=links)
+
+if __name__ == '__main__':
+	app.debug=True
+    app.run(host='0.0.0.0', port=80)
